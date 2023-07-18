b280a5922bf200cc8b6c77d36ef9b0a8b70ac432
Mayur Gowravaram <mayur.gowravaram@meraki.net>
2023-07-17 20:18:19 -0500

    proxy toggle

--- source/alias.sh
+++ source/alias.sh
@@ -14,3 +14,15 @@ alias szsh='source ~/.zshrc'
 
 # MITMProxy
 alias mmp='mitmproxy'
+
+proxytoggle() {
+  if [[ "$1" == "on" ]]; then
+    sudo networksetup -setwebproxystate Wi-Fi on
+    sudo networksetup -setsecurewebproxystate Wi-Fi on
+    echo "Web proxies turned on"
+  else
+    sudo networksetup -setwebproxystate Wi-Fi off
+    sudo networksetup -setsecurewebproxystate Wi-Fi off
+    echo "Web proxies turned off"
+  fi
+}
